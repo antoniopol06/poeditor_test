24f15ab5ba9c5b62122c2928f0b0274f4bfe093d
antoniopol06 <antonio.pol@liferay.com>
2017-02-28 16:44:00 +0100

    default englishtestwq

--- gulp-tasks/exportToPoEditor.js
+++ gulp-tasks/exportToPoEditor.js
@@ -11,21 +11,6 @@ function errorHandler(err) {
     this.emit('end');
 }
 
-gulp.task('export-to-poeditor', function (callback) {
 
-    if (!process.env.POEDITOR_API_KEY || !process.env.POEDITOR_PROJECT_ID) {
-        errorHandler(new Error('POEDITOR_API_KEY and POEDITOR_PROJECT_ID environment variables need to be set'));
-        return;
-    }
 
-    var lang = 'en';
 
-    var url = 'https://poeditor.com/api/webhooks/github?api_token=' + process.env.POEDITOR_API_KEY + '&id_project=' +
-        process.env.POEDITOR_PROJECT_ID + '&language=' + lang + '&operation=import_terms_and_translations&overwrite_translations=1';
-
-    request
-        .get(url)
-        .on('response', function() {
-            console.log('english exported');
-        });
-});
